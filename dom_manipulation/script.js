06ee791157433c54f1a50825640da2fc6426015b
manhathaway <alex.mannoia@gmail.com>
2023-06-09 20:36:30 -0700

    stuff

--- dom_manipulation/script.js
+++ dom_manipulation/script.js
@@ -65,3 +65,55 @@ for (let i = 0; i < span_container_children.length; i++) {
     spanStyling(span_container_children[i])
     console.log(i)
 }
+
+/*
+'addNums(arr, n)' will be a function that returns the sum of each item in an array, where the arguments signify:
+    - arr:  An arbitrary array.
+    - n:    The number of items in the array that will be summed together (arr[n - 1] + arr[n - 2] + ... + arr[n - n], where arr[n - 1] is the nth item in an array).
+Note that:
+    - addNums(arr, n)       =   addNums(arr, n - 1)         +   arr[n = 1].
+    - multiplyNums(arr, n)  =   multiplyNums(arr, n - 1)    *   arr[n - 1].
+This is what allows these types of recursion functions to work.
+*/
+
+// We initialize a variable 'box' that we will add each array item to.
+let box = 0;
+const addNums = (arr, n) => {
+    // Once this 'if statement' holds true, this marks the end of the 'loop', and outputs the sum of the array that we have stored in 'box'.
+    if (n <= 0) {
+      return box;
+    // Assuming (n > 0), arr[n - 1] is added to 'box' and the function calls itself, but reducing the value of n by 1 each time the function is called. This repeats until the above 'if statement' is no longer false.
+    // By decrementing the value of n by 1 each time the 'else statement' runs, we are adding each index of the array to 'box' until the above if statement holds true.
+    // What's really going on behind the scenes is: 0 + arr[4] + arr[3] + ... + arr[0].
+    } else {
+        box = box + arr[n - 1];
+        return addNums(arr, n - 1);
+    }
+  }
+console.log(addNums([1, 2, 3, 4, 5], 5));
+
+
+// 'rangeGlobal' returns an array with values between and inclusive of arguments 'lower' and 'upper'.
+// This function initializes a global variable 'countArray'.
+const globalArray = [];
+function rangeGlobal(lower, upper) {
+    if (lower == (upper + 1)) {
+      return globalArray;
+    } else {
+      globalArray.push(lower);
+      return rangeGlobal(lower + 1, upper);
+    }
+  }
+console.log(rangeGlobal(1, 10));
+
+// 'rangeLocal' does the same thing as 'rangeGlobal' except it doesn't rely on a global variable.
+function rangeLocal(lower, upper) {
+    if (lower == (upper + 1)) {
+      return [];
+    } else {
+      const arr = rangeLocal(lower + 1, upper)
+      arr.unshift(lower);
+      return arr;
+    }
+  }
+console.log(rangeLocal(1, 10));
